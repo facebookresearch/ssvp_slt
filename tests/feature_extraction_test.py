abfc3aea528b7de4ba8eba5783d03f99f2bc8ace
Joozef315 <m.youssef315@yahoo.com>
2024-11-04 21:33:26 +0200

    add LICENSE to new files
    Signed-off-by: Joozef315 <m.youssef315@yahoo.com>

--- tests/feature_extraction_test.py
+++ tests/feature_extraction_test.py
@@ -1,3 +1,14 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# BEiT: https://github.com/microsoft/unilm/tree/master/beit
+# --------------------------------------------------------
+
 import unittest
 from translation.feature_extraction_module import FeatureExtractionConfig, FeatureExtractionModule, LauncherConfig
 from unittest.mock import patch, MagicMock
