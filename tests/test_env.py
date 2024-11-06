17040e60b366967ff79a4ee40a9fc51a70e0a521
Joozef315 <m.youssef315@yahoo.com>
2024-11-06 14:49:51 +0200

    removing L6-10 in the header

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,13 +1,7 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-# --------------------------------------------------------
-# References:
-# DeiT: https://github.com/facebookresearch/deit
-# BEiT: https://github.com/microsoft/unilm/tree/master/beit
-# --------------------------------------------------------
 
 import torch
 import dlib
