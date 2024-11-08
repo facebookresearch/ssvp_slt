388dfc44c57fee1824d423099f5eee8604ec1da4
Joozef315 <m.youssef315@yahoo.com>
2024-11-08 16:55:35 +0200

    add LICENSE header

--- tests/translation_module_test.py
+++ tests/translation_module_test.py
@@ -1,3 +1,10 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+
 import unittest
 from unittest.mock import MagicMock, patch
 from translation.run_translation_module import TranslationModule, Config
